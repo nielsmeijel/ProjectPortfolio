d09a115672459623d4aa0ee2ad5147071c0ab8ce
nielsmeijel <123317387+nielsmeijel@users.noreply.github.com>
2023-07-08 04:33:45 +0200

    v50

---
+++ RecipeScraper/ReadMe.md
@@ -0,0 +1,18 @@
+
+## Recipe Scraper 👨‍🍳
+
+This is a simple web scraper that scrapes recipes from the website [Epicuious](https://www.epicurious.com/). It is written in Python and uses the [BeautifulSoup](https://www.crummy.com/software/BeautifulSoup/bs4/doc/) library to parse the HTML. The main function does not take any arguments as inputs and it returns a list of dictionaries. Each dictionary contains the following keys: "primarykey", "Title","Author","ContributorLink", "PublishDate","Serves","Ingredients","Methods". The return can easily be parsed to json or csv files accordingly. 
+
+The script scrapes the home pages newest recipes: "Our Newest Recipes" at the bottom of the page, it access those 5 new recipe and scrapes the recipe page for the information. The script does not scrape the entire website. The html structure of the website was manually inspected for the relvant information. 
+
+### Usage 
+
+The script can easily be used in an ETL process. The script can be scheduled to run daily and the output can be stored in a database.
+Personally i parsed the output to csv files and stored them in a folder locally. I have 
+
+### Limitations
+The script is dependent on the css and html structure of the website. If the website changes the script will not work.
+
+The script only scrapes epicurious.com. 
+
+
---
+++ RecipeScraper/dailyrecipescraper.py
@@ -0,0 +1,87 @@
+import requests
+from bs4 import BeautifulSoup
+import boto3
+import hashlib
+
+def links_to_newest_recipes(soup):
+    linkstorecipes = []
+    navnumpages = soup.find_all('div', class_="recipe")
+    for div in navnumpages:
+        recipelink = div.find('a')['href']
+        linkstorecipes.append(recipelink)
+    return linkstorecipes
+
+
+def scrape_recipe_content(recipeurl):
+    response = requests.get(recipeurl)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        header = soup.head.title.text
+        header = header.replace(" Recipe | Epicurious", "")
+
+        authors = soup.find('a', class_="BaseWrap-sc-gjQpdd BaseText-ewhhUZ BaseLink-eNWuiM BylineLink-gEnFiw iUEiRd dPcDNa hcRjeq eErqIx byline__name-link button")
+        contributorLink = authors['href']
+        authors = authors.text
+
+        publishDate = soup.find('time', class_="SplitScreenContentHeaderPublishDate-bMGEVk jZRIyI")['datetime']
+        
+        try:
+            serves = soup.find('p', class_="BaseWrap-sc-gjQpdd BaseText-ewhhUZ Yield-eyQhTA iUEiRd gQBfcy DyTuB")
+            serves = serves.text
+        except: 
+            serves = "N/A"
+
+
+        ingredients = soup.find_all('div', class_="Wrapper-dxnTBC jIWNsq")[0]
+        ingredients = ingredients.find_all('div', class_="BaseWrap-sc-gjQpdd BaseText-ewhhUZ Description-cSrMCf iUEiRd ioVvSX fsKnGI")
+        ingredients = [ingredient.text for ingredient in ingredients]
+        ingredientsDict = {index:ingredient for index, ingredient in enumerate(ingredients)}
+
+
+        methods = soup.find_all('div', class_="InstructionsWrapper-hZXqPx RmryN")[0]
+        methods = methods.find_all('p')
+        methods = [method.text for method in methods]
+        methodsDict = {index:method for index, method in enumerate(methods)}
+
+        
+
+        returnRecipeDict = {
+            "primarykey": hashlib.md5(header.encode()).hexdigest(),
+            "Title": header,
+            "Author": authors,
+            "ContributorLink": contributorLink,
+            "PublishDate": publishDate,
+            "Serves": serves,
+            "Ingredients": ingredientsDict,
+            "Methods": methodsDict,
+        }
+    else:
+        print("Error: ", response.status_code)
+    
+    return returnRecipeDict
+
+
+
+
+
+
+def main():
+
+    baseurl = "https://www.epicurious.com"
+    response = requests.get(baseurl)
+    print(response.status_code)
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    newestRecipes = []
+    for link in links_to_newest_recipes(soup):
+        
+        newestRecipes.append(scrape_recipe_content(baseurl + link))
+    print(newestRecipes)
+    return newestRecipes
+
+
+if __name__ == "__main__":
+    main()
